ea0190ae9d91c3ceb95c953fb78c639316c70654
Noldor010 <qwerrnoldor2@gmail.com>
2022-12-25 16:17:07 +0200

    start

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  basePath:"/Fudo-Pizzashop-Patreon",
+  assetPrefix:"/Fudo-Pizzashop-Patreon",
+}
+
+module.exports = nextConfig
